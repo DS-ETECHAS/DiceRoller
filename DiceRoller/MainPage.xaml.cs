85fb71ae8cec49bc99e33dd0a797a63528840be7
= <=>
2024-09-06 19:40:17 -0300

    Template

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -10,13 +10,6 @@ public partial class MainPage : ContentPage
     //Sortear um número aleatório através do clique do botão
     //Devolver o número sorteado para a interface
     //Conseguir selecionar a quantidade de lados a partir do Picker
-    private void SortearNumeroAleatorio(object sender, EventArgs e)
-    {
-        var maxValue = SeletorDeLadosDoDado.SelectedItem;
-        var numeroSorteado = new Random().Next(1, (int)maxValue);
-
-        NumeroSorteado.Text = numeroSorteado.ToString();
-    }
 
 }
 
